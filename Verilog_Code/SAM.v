3b7d4bebf0ea511ac402035733ea21191a79d934
Lee Jisoo <54274442+tojslee@users.noreply.github.com>
2019-12-13 15:17:41 +0900

    Update SAM.v

--- Verilog_Code/SAM.v
+++ Verilog_Code/SAM.v
@@ -39,113 +39,7 @@ module SAM();
     else if (ALU_PASS_B) ALU_RESULT = ALU_B;
   end
 
-  always @ ( negedge clk ) begin
-    // TODO: refer to lecture note, page 46
-    if (b[21]) begin
-      assign ABUS = PC;
-    end
-    
-    if (b[20]) begin
-      assign ABUS = PC;
-    end
-    
-    if (b[19]) begin
-      assign ABUS = IR;
-    end
-    
-    if (b[18]) begin
-      assign ABUS = MAR;
-    end
-    
-    if (b[17]) begin
-      assign AC = RBUS;
-    end
-    
-    if (b[16]) begin
-      assign ALU_A = AC;
-    end
-    
-    if (b[15]) begin
-      assign ALU_B = MBUS;
-    end
-    
-    if (b[14]) begin
-      assign ALU_ADD = 1;
-    end
-    else begin
-      assign ALU_ADD = 0;
-    end
-    
-    if (b[13]) begin
-      assign ALU_PASS_B = 1;
-    end
-    else begin
-      assign ALU_PASS_B = 0;
-    end
-    
-    if (b[12]) begin
-      assign ADDRESS_BUS = MAR;
-    end
-    
-    if (b[11]) begin
-      assign DATA_BUS = MBR;
-    end
-    
-    if (b[10]) begin
-      assign IR = ABUS;
-    end
-    
-    if (b[9]) begin
-      assign MAR = ABUS;
-    end
-    
-    if (b[8]) begin
-      assign MBR = DATA_BUS;
-    end
-    
-    if (b[7]) begin
-      assign MBR = RBUS;
-    end
-    
-    if (b[6]) begin
-      assign PC = 0;
-    end
-    
-    if (b[5]) begin
-      assign PC = PC + 2'b10;
-    end
-    
-    if (b[4]) begin
-      assign PC = ABUS;
-    end
-    
-    if (b[3]) begin
-      assign RW = 1;
-    end
-    else begin
-      assign RW = 0;
-    end
-    
-    if (b[2]) begin
-      assign REQUEST = 1;
-    end
-    else begin
-      assign REQUEST = 0;
-    end
-    
-    if (b[1]) begin
-      assign RBUS = AC;
-    end
-    
-    if (b[0]) begin
-      assign RBUS = ALU_RESULT;
-    end
-  end
-  // TODO: refer to lecture note, page 8 and write additional code here
-  
-  
-  /*
-  always @ ( negedge clk ) begin
+    always @ ( negedge clk ) begin
      if (b[21]) begin
       assign ABUS = PC;
     end
@@ -245,8 +139,9 @@ module SAM();
     if (b[0]) begin
       assign RBUS = ALU_RESULT;
     end
-  
-  */
+  end
+  // TODO: refer to lecture note, page 8 and write additional code here
   
 endmodule
+
 
