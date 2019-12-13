a38667abdf08ef89e32eb13556f792d90fd0ad62
Lee Jisoo <54274442+tojslee@users.noreply.github.com>
2019-12-13 22:01:42 +0900

    Add files via upload

--- final_project_template/SAM.v
+++ final_project_template/SAM.v
@@ -3,14 +3,17 @@
 module SAM();
   reg clk;
 
-  reg [15:0] PC, AC, MAR, MBR, IR;
+// TODO: you may alter the type of registers to wire (e.g. reg RW -> wire RW) if necessary
+  reg [15:0] PC;
+  reg [15:0] AC, MAR, MBR, IR;
   reg [15:0] ABUS, RBUS, MBUS;
   reg [15:0] ADDRESS_BUS, DATA_BUS;
   reg RW, REQUEST;
   wire WAIT;
 
-  reg [15:0] ALU_A, ALU_B, ALU_RESULT;
+  reg [15:0] ALU_A, ALU_B;
   reg ALU_ADD, ALU_PASS_B;
+  reg [15:0] ALU_RESULT;
 
   wire [21:0] b;
   controller my_controller(clk, WAIT, IR[15], AC[15], IR[14], b);
@@ -20,12 +23,11 @@ module SAM();
   Memory my_memory(ADDRESS_BUS, REQUEST, RW, WAIT, DATA_BUS, data_bus_t);
 
   // initial settings
+  // TODO : add any initialization process if required (not necessary)
   initial begin
     clk = 0;
     AC = 0;
     IR = 0;
-    RW = 1;
-    REQUEST = 1;
     ADDRESS_BUS = 0;
   end
   always begin
@@ -40,19 +42,23 @@ module SAM();
 
   always @ ( negedge clk ) begin
     // TODO: refer to lecture note, page 46
-
-
-
-
+/* Example 1. clk-synchrinized implementation
+    if		(b[21]) ABUS = PC;
+    if		(b[20]) ABUS = IR;
+    if		(b[19]) ABUS = MBR;
+*/
   end
 
-  // TODO: refer to lecture note, page 8 and write additional code here
+/* Example 2. register without clk
+  always @ (b or PC or IR or MBR) begin
+    if (b[21]) ABUS = PC;
+    if (b[20]) ABUS = IR;
+    if (b[19]) ABUS = MBR;
+  end
+*/
 
-
-
-
-
-
-
-  
+/* Example 3. wire-based
+  assign ABUS = b[21] ? PC : (b[20] ? IR : (b[19] ? MBR : 'bz));
+  Note that you need to change ABUS's type as wire in this case (wire ABUS)
+*/
 endmodule
